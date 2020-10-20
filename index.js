dfc81819f8f89b1a9704ca435a69af522d97b86a
Chloe Steffens <caforonda103@yahoo.com>
2020-10-20 05:18:04 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase => titleCase.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
 }
+
